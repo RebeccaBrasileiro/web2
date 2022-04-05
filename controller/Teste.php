23b8dba40e1df45c0af140654531362a66d2709b
RebeccaBrasileiro <rebeccabrasileiro28@gmail.com>
2022-04-05 07:11:38 -0300

    web

---
+++ controller/Teste.php
@@ -0,0 +1,10 @@
+<?php
+    class Teste
+ {
+   public function __construct()
+   {
+       echo '<h1>Teste</h1>';
+   }
+ }
+
+ ?>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+   //auto load
+   spl_autoload_extensions('.php');
+   function classLoader($class)
+    {
+       $nomeArquivo = $class . '.php';
+       $pastas = array('controller','model');
+       foreach ($pastas as $pastas){
+           $arquivo = "{$pastas}/{$nomeArquivo}";
+           if (file_exists($arquivo)) {
+               require_once($arquivo);
+           }
+           
+       }
+    }
+    spl_autoload_register('classLoader');
+
+    //Front Controller
+
+    class Aplicacao
+    {
+        public static function run ()
+        {
+            if(isset($_GET["acao"])) {
+                echo $_GET["acao"];
+            }
+        }
+    }
+    Aplicacao::run();
+?>
