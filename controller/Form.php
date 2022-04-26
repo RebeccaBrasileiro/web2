f1aaba8346084f92f8ddc20a9372889ae6241a76
RebeccaBrasileiro <rebeccabrasileiro28@gmail.com>
2022-04-26 08:34:40 -0300

    web

--- controller/Form.php
+++ controller/Form.php
@@ -2,6 +2,10 @@
 class Form
 {
   private $message = "";
+  public function __construct()
+  {
+    Transaction::open();
+  }
   public function controller()
   {
     $form = new Template("view/form.html");
@@ -9,10 +13,25 @@ class Form
   }
   public function salvar()
   {
-    print_r($_POST);
+    if (isset($_POST["nome"]) && isset($_POST["mensagem"]) && isset($_POST["datahora"])) {
+      try {
+        $conexao = Transaction::get();
+        $mural = new Crud("mural");
+        $nome = $conexao->quote($_POST["nome"]);
+        $mensagem = $conexao->quote($_POST["mensagem"]);
+        $datahora = $conexao->quote($_POST["datahora"]);
+        $resultado = $mural->insert("nome, mensagem, datahora", "$nome, $mensagem, $datahora");
+      } catch (Exception $e) {
+        echo $e->getMessage();
+      }
+    }
   }
   public function getMessage()
   {
     return $this->message;
+  }
+  public function __destruct()
+  {
+    Transaction::close();
   }
 }
--- controller/Tabela.php
+++ controller/Tabela.php
@@ -2,12 +2,39 @@
 class Tabela
 {
   private $message = "";
+  public function __construct()
+  {
+    Transaction::open();
+  }
   public function controller()
   {
-    $this->message = "Estou na classe Tabela";
+    Transaction::get();
+    $mural = new Crud("mural");
+    $resultado = $mural->select();
+    $tabela = new Template("view/tabela.html");
+
+    if (is_array($resultado)) {
+      $tabela->set("linha", $resultado);
+      $this->message = $tabela->saida();
+    }
+  }
+  public function remover()
+  {
+    try {
+      $conexao = Transaction::get();
+      $id = $conexao->quote($_GET["id"]);
+      $mural = new Crud("mural");
+      $resultado = $mural->delete("id = $id");
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
   }
   public function getMessage()
   {
     return $this->message;
+  }
+  public function __destruct()
+  {
+    Transaction::close();
   }
 }
